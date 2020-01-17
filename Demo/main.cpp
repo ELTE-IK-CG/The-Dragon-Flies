fa25ace13dc89eaa4153764114c1b14eaf12ad37
Csabix <csabix.balint@gmail.com>
2020-01-17 20:37:13 +0100

    Changed to use DragonflyPack

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,58 +1,47 @@
 #include <Dragonfly/editor.h>
 #include <Dragonfly/detail/buffer.h> //will be replaced
 #include <Dragonfly/detail/vao.h> //will be replaced
-#include <Dragonfly/detail/Framebuffer/Framebuffer.h> //will be replaced
-#include <GL/glew.h>
-#include <vector>
-
-using namespace df;
 
 int main(int argc, char* args[])
 {
-	Sample sam; //handles Events and such
-	Camera cam;
+	df::Sample sam; //handles Events and such
+	df::Camera cam;
 	sam.AddHandlerClass(cam, 5);
-	sam.AddStaticHandlerClass<ImGuiHandler>(10);
+	sam.AddStaticHandlerClass<df::ImGuiHandler>(10);
 
-	eltecg::ogl::ArrayBuffer MyVBO;
-	MyVBO.constructMutable(std::vector<glm::vec2>{ {-1, -1}, { 1, -1 }, { 0, 1 }}, GL_STATIC_DRAW);
+	eltecg::ogl::ArrayBuffer MyVBO;	MyVBO.constructMutable(std::vector<glm::vec2>{ {-1, -1}, { 1, -1 }, { 0, 1 }}, GL_STATIC_DRAW);
+	eltecg::ogl::VertexArray MyVAO;	MyVAO.addVBO<glm::vec2>(MyVBO);		//these two classes will be removed from Dragonfly as soon as we have the replacement ready
 
-	eltecg::ogl::VertexArray MyVAO;
-	MyVAO.addVBO<glm::vec2>(MyVBO);
+	df::VaoArrays demoVao((GLuint)MyVAO, GL_TRIANGLE_STRIP, 3, 0u); // temporary solution that wraps an ID
 
-	VaoArrays demoVao((GLuint)MyVAO, GL_TRIANGLE_STRIP, 3, 0u); // temporary solution
-	NoVao noVao = NoVao(GL_TRIANGLES, 3);
+	df::TextureCube<> testCubemap("Assets/xpos.png", "Assets/xneg.png", "Assets/ypos.png", "Assets/yneg.png", "Assets/zpos.png", "Assets/zneg.png");
+	df::Texture2D<> testTex = testCubemap[df::X_POS]; // 2D view of a cubemap face
 
-	TextureCube<> testCubemap("Assets/xpos.png", "Assets/xneg.png", "Assets/ypos.png", "Assets/yneg.png", "Assets/zpos.png", "Assets/zneg.png");
-	Texture2D<> testTex = testCubemap[TextureType::TEX_CUBE_X_POS]; // 2D view of a cubemap face
+	df::ShaderProgramEditorVF program = "MyShaderProgram";
+	program << "Shaders/vert.vert"_vert << "Shaders/frag.frag"_frag << df::LinkProgram;
+	df::ShaderProgramEditorVF postprocess = "Postprocess shader program";
+	postprocess << "Shaders/postprocess.vert"_vert << "Shaders/postprocess.frag"_frag << df::LinkProgram;
 
-	ShaderProgramEditorVF program = "MyShaderProgram";
-	program << "Shaders/vert.vert"_vs << "Shaders/frag.frag"_fs << LinkProgram;
-	ShaderProgramEditorVF postprocess = "Postprocess shader program";
-	postprocess << "Shaders/postprocess.vert"_vs << "Shaders/postprocess.frag"_fs << LinkProgram;
+	int w = df::Backbuffer.getWidth(), h = df::Backbuffer.getHeight();
+	auto frameBuff = df::Renderbuffer<df::depth24>(w, h) + df::Texture2D<>(w, h, 1);
 
-	auto frameBuff = Renderbuffer<depth24>(Backbuffer.getWidth(), Backbuffer.getHeight()) + Texture2D<>(Backbuffer.getWidth(), Backbuffer.getHeight(), 1);
-
-	sam.AddHandlerClass(Backbuffer);
 	sam.AddResize([&](int w, int h) {frameBuff = frameBuff.MakeResized(w, h); });
 	
 	GL_CHECK; //extra opengl error checking in GPU Debug build configuration
 
-	sam.Run([&](float deltaTime)
+	sam.Run([&](float deltaTime) //delta time in ms
 		{
 			cam.Update();
 
-			frameBuff << ClearColor<0>(0.f, 0.1f, 0.5f) << ClearDepth() << program << "texImg" << testTex;
+			frameBuff << df::Clear() << program << "texImg" << testTex;
 			program << demoVao;	//Rendering: Ensures that both the vao and program is attached
 
-			Backbuffer << ClearColor(0.f, 0.1f, 0.5f) << ClearDepth() << postprocess << "texFrame" << frameBuff.getColor<0>();
-			postprocess << noVao;
+			df::Backbuffer << df::Clear() << postprocess << "texFrame" << frameBuff.get<glm::u8vec3>();
+			postprocess << df::NoVao(GL_TRIANGLES, 3); // Rendering a pixel shader
 
 			GL_CHECK;
-			program.Render(); //only the UI!!
-			postprocess.Render();
+			program.Render(); postprocess.Render(); //only the UI!!
 		}
 	);
 	return 0;
 }
-
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # The-Dragon-Flies
-Template repository for the use of the Dragonfly framework
+
+This is a template repository for the Dragonfly framework.
+
+## More Information
+
+ - [Dragonfly Poster](https://people.inf.elte.hu/csabix/publications/articles/WSPS_2020_Poster_Dragonfly.pdf)
+ - [Dragonfly Project](https://github.com/Csabix/Dragonfly)
+ - Or contact the authors at csabix@inf.elte.hu or bundas@inf.elte.hu
+ 
+## Install
+
+ - Download the [DragonflyPack.zip](https://drive.google.com/file/d/1Ip1ilcjGu7oTZ3ixYK0olPBVj-89Wr9G/view?usp=sharing)
+ - Find a location and create a virtual T drive, eg.: `subst T: C:\MyT_Drive\`
+ - Unzip the `DragonflyPack` there. There should be 3 folders in `T:\DragonflyPack`
+ - Download Dragonfly as a submodule within the cloned The-Dragon-Flies project
+ - Use Visual Studio to build the solution
+
+## Credits
+
+Dragonfly is developed by Csaba Bálint and Róbert Bán at Eötvös Loránd University, Budapest, Hungary.
+
+Supported by the ÚNKP19-3 New National Excellence Program of the Ministry for Innovation and Technology. The project has been supported by the European Union, co-financed by the European Social Fund (EFOP-3.6.3-VEKOP-16-2017-00001).
+
+Dragonfly is licensed under the MIT License, see `Dragonfly\LICENSE.txt` for more information.
